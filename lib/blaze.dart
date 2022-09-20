fe952e2fd48594f965c4df83244a76b9388d1e9a
22726020 <22726020@edu.cc.saga-u.ac.jp>
2022-09-20 13:53:02 +0900

    url機能追加

---
+++ lib/blaze.dart
@@ -0,0 +1,139 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:photograph/display.dart';
+import 'package:photograph/get.dart';
+import 'package:quiver/async.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+//git追加テスト
+class BlazePage extends StatefulWidget {
+  const BlazePage({Key? key, required this.camera,required this.imagePath}) : super(key: key);
+  final CameraDescription camera;
+  final String imagePath;
+
+  //パスも一緒に受け取る
+  @override
+  State<BlazePage> createState() => _BlazePageState();
+}
+
+class _BlazePageState extends State<BlazePage> {
+  bool _fileSelected = false;
+  bool _poseFound = false;
+  var listx = [];
+  var listy = [];
+  bool hantei = true;
+
+  String? _extension;
+  String? _fileName;
+  File? _filePath;
+  var offsets = <Offset>[];
+
+  final poseDetector = PoseDetector(options: PoseDetectorOptions());
+
+  Future<void> _blazePose() async {
+    String path_1 = widget.imagePath;//
+    final inputImage = InputImage.fromFilePath(path_1);
+    final List<Pose> poses = await poseDetector.processImage(inputImage);
+
+
+    for (Pose pose in poses) {
+      // to access all landmarks
+      pose.landmarks.forEach((_, landmark) {
+        offsets.add(Offset(landmark.x, landmark.y));
+        print("${landmark.type}, x=${landmark.x}, y=${landmark.y}");
+
+        listx.add(landmark.x);
+        listy.add(landmark.y);
+      });
+    }
+    print(List<Pose>);
+    setState(() {
+      _poseFound = true;
+    });
+    
+    //閾値をピクセル値以上に設定する
+    var EllorXlist = listx.where((value) => value > 700000).toList();
+    var EllorYlist = listy.where((value2) => value2 > 7000000).toList();
+    print(listx);
+    print(listy);
+    print(EllorXlist);
+    print(EllorYlist);
+    hantei = false;
+    //おかしいところ判定
+    if (EllorXlist.isEmpty && EllorYlist.isEmpty) {
+      hantei = true;
+      //trueの時保存するコード書く
+    }
+    print(hantei);
+    
+  }
+
+  void _resetState() {
+    if (!mounted) {
+      return;
+    }
+    setState(() {
+      _fileSelected = false;
+      _poseFound = false;
+      _filePath = null;
+    });
+    
+  }
+
+  Widget _showImage() {
+    if (_fileSelected) {
+      return Image.file(_filePath!);
+    } else {
+      return const Text('');
+    }
+  }
+
+  Widget _drawPoints() {
+    if (_poseFound) {
+      return CustomPaint(
+      );
+    } else {
+      return const Text('');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (hantei == false)
+      Future.delayed(
+        Duration.zero,
+        () => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("姿勢がうまく取れていません"),
+            content: Text("やり直してください"),
+            actions: [
+              TextButton(
+                child: Text("閉じる"),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        ),
+      );
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              //勝手にボタン押せるようにする
+              onPressed: () => _blazePose(),
+              child: const Text('Run BlazePose'),
+              
+            ),
+            Stack(
+              children: <Widget>[_showImage(), _drawPoints()],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:photograph/display.dart';
+import 'package:photograph/get.dart';
+import 'package:quiver/async.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> main() async {
+  // main 関数内で非同期処理を呼び出すための設定
+  WidgetsFlutterBinding.ensureInitialized();
+  // デバイスで使用可能なカメラのリストを取得
+  final cameras = await availableCameras();
+  // 利用可能なカメラのリストから特定のカメラを取得
+  final firstCamera = cameras.first;
+  runApp(MyApp(camera: firstCamera));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+  final CameraDescription camera;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  MyHomePage(camera:camera,title: '抱っこアプリ'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.camera,required this.title}) : super(key: key);
+
+  final String title;
+  final CameraDescription camera;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(centerTitle: true,title: new Text(widget.title,style:TextStyle(color: Colors.black)),
+        backgroundColor: Color.fromARGB(255, 174, 168, 167)),
+        body: new Stack(
+          children: <Widget>[
+            new Container(
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: new NetworkImage(
+                      "https://www.angeliebe.co.jp/html/upload/save_image/product/50460/50460-190_l4.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 32),
+              child: Text('選択してください',style: TextStyle(fontSize: 30),),
+            ),
+           
+           SizedBox(height: 150,),
+            Row(
+              
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 214, 204, 203),
+                    elevation: 26,
+                  ),
+                  child: Text('基本情報入力',style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  onPressed: _opneUrl,
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 214, 204, 203),
+                    elevation: 26,
+                  ),
+                  child: Text('アンケート入力',style: TextStyle(color: Colors.black)),
+                ),
+                // ElevatedButton(
+                //   onPressed: () {},
+                //   style: ElevatedButton.styleFrom(
+                //     primary: Color.fromARGB(255, 214, 204, 203),
+                //     elevation: 26,
+                //   ),
+                //   child: Text('抱っこ撮影',style: TextStyle(color: Colors.black),),
+                // ),
+                ElevatedButton(
+                  onPressed: (){
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TakePictureScreen(camera:widget.camera),
+              )
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 214, 204, 203),
+                    elevation: 16,
+                  ),
+                  child: Text('抱っこ撮影',style: TextStyle(color: Colors.black)),
+                ),
+              ],
+            ),
+          ],
+        ),
+            // ElevatedButton(
+            //   onPressed: () { },
+            //   child: Text('基本情報の登録',style:TextStyle(color: Colors.black)),
+            //   )
+          ],
+        )
+    );
+  }
+    void _opneUrl() async {
+    final url = Uri.parse('https://docs.google.com/forms/d/168uxibsGbr7ciBM2FsLRksgxEMEkJ_TzCcePeq9n82s/edit?usp=sharing'); //←ここに表示させたいURLを入力する
+    if (await canLaunchUrl(url)) {
+      await launchUrl(
+        url,
+      );
+    } else {
+      throw 'このURLにはアクセスできません';
+    }
+  }
+}
